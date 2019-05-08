626b3f2df9972a7686d8798f96a93d1a60fcef7a
akashraut1 <47948463+akashraut1@users.noreply.github.com>
2019-05-08 10:40:30 +0530

    Add files via upload

---
+++ file open read.py
@@ -0,0 +1,20 @@
+fo = open("Akash.txt", "rw+")
+#print ("Name of the file: ", fo.name)
+
+# Assuming file has following 5 lines
+# This is 1st line
+# This is 2nd line
+# This is 3rd line
+# This is 4th line
+# This is 5th line
+
+line = fo.readline()
+print ("Read Line: %s" % (line))
+
+# Again set the pointer to the beginning
+fo.seek(0, 0)
+line = fo.readline()
+print ("Read Line: %s" % (line))
+
+# Close opend file
+fo.close()
