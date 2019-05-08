d8192343f8565644fd0678f3d471dfd29ab3eae6
akashraut1 <47948463+akashraut1@users.noreply.github.com>
2019-05-08 10:55:17 +0530

    Add files via upload

---
+++ Basic Python/open files.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  2 11:09:52 2019
+
+@author: Akash
+"""
+
+file = open("Akash.txt","r") 
+a,b=file.readline(),file.readline()
+print(a,b)
---
+++ Basic Python/string basic using Function.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  2 10:31:43 2019
+
+@author: Akash
+"""
+def fun(Name,Surname):
+    a=name+surname
+    return a
+name= input("what is your name")
+surname=input("what is your surname")
+b = fun(name,surname)
+print(b)
+    
+    
